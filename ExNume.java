801403eabcb08991dc01be2d4d2b79f4c1848abc
FlorinWfmx <93483958+FlorinWfmx@users.noreply.github.com>
2021-11-04 12:07:12 +0200

    Update ExNume.java

--- ExNume.java
+++ ExNume.java
@@ -1,7 +1,7 @@
 public class ExNume{
     public static void main (String[]args){
-        Nume n1 = new Nume ("Ion");
-        Nume n2 = new Nume ("Popescu");
+        Nume n1 = new Nume ("Popescu");
+        Nume n2 = new Nume ("Ion");
         String s = n1.setNume(n2);
         System.out.println (s);
     }
